9decfcbe860dd55c5722f5fbd5805403aeb22281
Lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-04-15 23:53:01 +1200

    Create type.js

---
+++ type.js
@@ -0,0 +1,10 @@
+// @type.js
+const util = require("./javascript/util"
+const type = require(util.typename)
+
+type.input("x")
+type.prefix("https")
+type.get("github.com"
+         .log("Started")
+         
+util.log(type.output)
