92d57a4a4ffad64bc0db07e207ba57dfe2f033f0
Lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-04-16 23:50:03 +1200

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const express = require(”express”)
 
 const server = express()
 
+server.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
 
+server.listen(8080, () =>
+  console.log(`Example app listening on port 8080!`),
+);
