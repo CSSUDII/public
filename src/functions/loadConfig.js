93c88341a454de661d7b8657767c3b2451810368
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-04-23 23:33:06 +1200

    Add Real Code

--- src/functions/loadConfig.js
+++ src/functions/loadConfig.js
@@ -1,1 +1,13 @@
+import fs from "fs";
 
+async loadConfig(yaml, logger) => {
+    try {
+        await this.config = yaml.load(this.fs.readFileSync('../../config.yml', 'utf8'));
+        this.logger.info('Loaded Config File');
+    } catch (e) {
+        this.logger.error('Error loading Config ' + e);
+        this.logger.warn('Using Default Settings');
+    };
+};
+
+export default loadConfig;
