8f7fd70add2c012d3463f3e4431aa1e814efe4b4
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-04-23 23:01:36 +1200

    📝 Code Formatting

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // WE-BT16 API
-
 const express = require('express');
 const yaml = require('js-yaml');
 const logDoc = require('inklog.js');
@@ -9,12 +8,12 @@ this.fs = require('fs')
 
 // Load Config
 async loadConfig() => {
-try {
- await this.config = yaml.load(this.fs.readFileSync('config.yml', 'utf8'));
-  this.logger.info('Loaded Config File')
-} catch (e) {
-  this.logger.error('Error loading Config, Using Defaults' + e)
-};
+    try {
+        await this.config = yaml.load(this.fs.readFileSync('config.yml', 'utf8'));
+        this.logger.info('Loaded Config File')
+    } catch (e) {
+        this.logger.error('Error loading Config, Using Defaults' + e)
+    };
 }
 
 loadConfig();
@@ -23,18 +22,20 @@ this.port = this.config.port;
 this.debug = this.config.debug;
 
 async checks() => {
-  this.logger.debug('Running Checks');
-  if (!this.port) this.port = this.default.port;
-  if (!this.debug) this.debug = false;
+    this.logger.debug('Running Checks');
+    if (!this.port) this.port = this.default.port;
+    if (!this.debug) this.debug = false;
 };
 
 const server = express();
 
 server.get('/', (req, res) => {
-  if (this.debug) { this.logger.debug('POST Request Received') }
-  return res.send('Received a GET HTTP method');
+    if (this.debug) {
+        this.logger.debug('POST Request Received')
+    }
+    return res.send('Received a GET HTTP method');
 });
 
 server.listen(this.port, () => {
-  this.logger.info(`API Live on: ${this.port}`),
+    this.logger.info(`API Live on: ${this.port}`),
 });
