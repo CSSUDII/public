dbd04a7e904135de65066ee430da81394b59af88
Lucaslah <lucas.petrino@spycampllc.ml>
2021-04-29 10:58:46 +1200

    Merge Changes

---
+++ frontend/build/precache-manifest.dee6842534038ad9a35093670eac27d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d85008cc291cc01e109a1bbcce41f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc649b77a2e61fba4620",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "c4a4cd021d0938b025ca",
+    "url": "/static/css/main.a02a33bf.chunk.css"
+  },
+  {
+    "revision": "cc649b77a2e61fba4620",
+    "url": "/static/js/2.4e43c3d9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4e43c3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a4cd021d0938b025ca",
+    "url": "/static/js/main.fd3019c9.chunk.js"
+  },
+  {
+    "revision": "6be9141813de8c8920f9",
+    "url": "/static/js/runtime-main.13fb96be.js"
+  }
+]);
