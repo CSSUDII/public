dd91b937385ce09bf203d68f6a1e5716a127afc1
Lucaslah <lucas.petrino@spycampllc.ml>
2021-04-30 19:07:21 +1200

    build(lib): update build files

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,5 +1,8 @@
 import express from "express";
 
+import helmet from "helmet";
+import hsts from "hsts";
+
 import indexRouter from "./routes/indexRouter";
 import placeholdersRouter from "./routes/placeholdersRouter";
 import UsersRouter from "./routes/UserRouter";
@@ -11,6 +14,22 @@ class Server {
         server.use('/', indexRouter);
         server.use('/v1/placeholders', placeholdersRouter);
         server.use('/v1/auth', UsersRouter);
+
+        // Security Stuff
+        server.use(helmet());
+
+        const hstsMiddleware = hsts({
+            maxAge: 1234000
+        });
+
+        server.use((req, res, next) => {
+            if (req.secure) {
+                hstsMiddleware(req, res, next)
+            } else {
+                next()
+            }
+        })
+
     };
 };
 
