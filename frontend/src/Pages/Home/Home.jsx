8f8999e2cd5e9f507a5ed8fed12d5f68479b2564
Lucaslah <lucas.petrino@spycampllc.ml>
2021-06-18 17:00:43 +1200

    Code Format

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Media } from "react-bootstrap";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -7,24 +7,36 @@ class Home extends Component {
 
   render() {
     return (
-        <>
-            <React.Fragment>
-                <div>
-                    <center>
-                        <h1>CSSUDII API</h1>
-                        <h1>CSSUDII API</h1>
-                    </center>
-                </div>
-            </React.Fragment>
-        </>
+      <>
+        <React.Fragment>
+          <div>
+            <center>
+              <h1>CSSUDII API</h1>
+              <h1>CSSUDII API</h1>
+            </center>
+          </div>
+        </React.Fragment>
+        <Media>
+          <img
+            width={64}
+            height={64}
+            className="mr-3"
+            src="holder.js/64x64"
+            alt="Generic placeholder"
+          />
+          <Media.Body>
+            <h5>Example Heading</h5>
+            <p>
+              Example
+            </p>
+          </Media.Body>
+        </Media>
+      </>
     );
   }
-
 }
 
-
 export default () => {
-  return (
-    <Home />
-)
+  return <Home />;
 };
+
