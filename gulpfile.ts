9ed974f83f6c3cc77eaf273a415bc85478cca96b
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2022-07-21 14:21:12 +0000

    feat: babel support

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import gulp from "gulp";
 import babel from "gulp-babel";
 import ts from "gulp-typescript";
@@ -6,46 +7,34 @@ import uglify from "gulp-uglify";
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("build:compile-ts", async () => {
+gulp.task("build:compile", async () => {
     return gulp
         .src("src/**/*.ts")
         .pipe(
             babel({
+                presets: ["@babel/env", "@babel/typescript"],
                 plugins: [
-                    //"@babel/plugin-proposal-decorators",
-                    //"@babel/proposal-class-properties",
-                    //"@babel/proposal-object-rest-spread",
-                    //"transform-decorators-legacy",
-                    //"transform-class-properties",
                     // @ts-ignore
-                    [
-                        "@babel/plugin-proposal-decorators",
-                        {
-                            legacy: true,
-                        },
-                    ],
-                    "@babel/plugin-proposal-class-properties",
+                    ["@babel/plugin-proposal-decorators", { legacy: true }],
+                    // @ts-ignore
+                    ["@babel/plugin-proposal-class-properties"],
                 ],
-                presets: ["@babel/preset-env", "@babel/preset-typescript"],
             })
         )
         .pipe(uglify())
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("build:type-check", async () => {
+gulp.task("build:types", async () => {
     return gulp.src("src/**/*.ts").pipe(tsProject());
 });
 
-gulp.task("clean:remove-dist", async () => {
+gulp.task("clean", async () => {
     await del("dist/**");
 });
 
 gulp.task("watch", async () => {
-    gulp.watch("src/**/*.ts", gulp.series("build:compile-ts"));
+    gulp.watch("src/**/*.ts", gulp.series("build:compile"));
 });
 
-gulp.task(
-    "default",
-    gulp.series("build:type-check", "clean:remove-dist", "build:compile-ts")
-);
+gulp.task("default", gulp.series("clean", "build:compile"));
